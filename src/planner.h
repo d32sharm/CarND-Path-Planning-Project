8ac4bbad36b521d2ef2c3e02df7af85eff5775f5
Dhruv Sharma <dhruvsharma@Dhruvs-MBP.lan>
2018-04-23 10:33:03 -0400

    adding path planning code and spline library header file

---
+++ src/planner.h
@@ -0,0 +1,31 @@
+#include <fstream>
+#include <math.h>
+#include <uWS/uWS.h>
+#include <chrono>
+#include <iostream>
+#include <thread>
+#include <vector>
+#include "Eigen-3.3/Eigen/Core"
+#include "Eigen-3.3/Eigen/QR"
+#include "json.hpp"
+#include "spline.h"
+
+
+using namespace std;
+
+// for convenience
+using json = nlohmann::json;
+
+// For converting back and forth between radians and degrees.
+constexpr double pi();
+double deg2rad(double x); 
+double rad2deg(double x); 
+
+
+string hasData(string s);
+double distance(double x1, double y1, double x2, double y2);
+int ClosestWaypoint(double x, double y, const vector<double> &maps_x, const vector<double> &maps_y);
+int NextWaypoint(double x, double y, double theta, const vector<double> &maps_x, const vector<double> &maps_y);
+vector<double> getFrenet(double x, double y, double theta, const vector<double> &maps_x, const vector<double> &maps_y);
+vector<double> getXY(double s, double d, const vector<double> &maps_s, const vector<double> &maps_x, const vector<double> &maps_y);
+void run();
